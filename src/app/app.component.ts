9a4fb0272944a5d2aa0a9cb5daee1d94ffc20da3
Muso Verda <gearmobile@gmail.com>
2024-09-08 15:11:14 +0300

    feat: :sparkles: Add directive functionality

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,17 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { SimpleCardComponent } from './components/simple-card/simple-card.component';
+import { HightlightDirective } from './directives';
 
 @Component({
   selector: 'dr-root',
   standalone: true,
-  imports: [SimpleCardComponent],
+  imports: [SimpleCardComponent, HightlightDirective],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class AppComponent {
-  //
+  public onHightLightChange(event: boolean): void {
+    console.log(event);
+  }
 }
--- src/app/directives/hightlight.directive.ts
+++ src/app/directives/hightlight.directive.ts
@@ -1,9 +1,30 @@
-import { Directive } from '@angular/core';
+import { Directive, EventEmitter, HostBinding, HostListener, Input, Output } from '@angular/core';
 
 @Directive({
   selector: '[hightlight]',
   standalone: true,
 })
 export class HightlightDirective {
-  constructor() {}
+  @Input('hightlight')
+  public hightlight = false;
+
+  @Output()
+  public hightLightChange = new EventEmitter<boolean>();
+
+  @HostBinding('class.hightlight')
+  get class() {
+    return this.hightlight ? 'hightlight' : '';
+  }
+
+  @HostListener('mouseover')
+  public onMouseOver() {
+    this.hightlight = true;
+    this.hightLightChange.emit(this.hightlight);
+  }
+
+  @HostListener('mouseleave')
+  public onMouseLeave() {
+    this.hightlight = false;
+    this.hightLightChange.emit(this.hightlight);
+  }
 }
